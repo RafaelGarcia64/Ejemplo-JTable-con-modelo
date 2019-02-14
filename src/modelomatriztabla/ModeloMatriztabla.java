f2cdca1b626f4a7c7b4b19d0a40f8d842ce4ca13
Rafael Garcia <regestra64@gmail.com>
2019-02-14 08:19:46 -0600

    init

---
+++ src/modelomatriztabla/ModeloMatriztabla.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelomatriztabla;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ModeloMatriztabla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmCandidatos frame = new frmCandidatos();
+        frame.setVisible(true);
+    }
+    
+}
